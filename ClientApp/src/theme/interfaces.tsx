4f47257170d15560f56dd16f712cbc44eef4de23
LBlanka99 <losonci.blanka@mensa.hu>
2023-12-08 17:05:51 +0100

    create model interfaces for frontend

---
+++ ClientApp/src/theme/interfaces.tsx
@@ -0,0 +1,26 @@
+export interface User {
+    id: string;
+    userName: string;
+    email: string | null;
+    password: string;
+    profilPicture: string | null;
+    roles: string[];
+};
+
+export interface Tag {
+    id: string;
+    name: string;
+    color: string;
+}
+
+export interface Task {
+    id: string;
+    title: string;
+    deadline: Date;
+    assignees: User[];
+    points: Number;
+    tags: Tag[];
+    description: string | null;
+    status: Number;
+    confirmingPhoto: string | null;
+}
